db4f724eec9bdec1b49e39c5d369977c6c5dd545
Zohaib Nawaz Tarar <zohaib_nawaz786@hotmail.com>
2019-11-24 14:00:16 +0000

    Added bootstrap from CDN and Included header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,9 +10,11 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <!-- Styles -->
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+@include('includes.header')
+
 <div class="container">
     @yield('content')
 </div>
