b436f62d86eb1b4d934b0f4695a512f1637778e3
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2019-12-21 13:01:45 +0000

    Added Middleware and route protection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,11 @@ class UserController extends Controller
     }
     public function postSignUp(Request $request)
     {
-
+        $this->validate($request, [
+           'email' => 'required|email|unique:users',
+            'first_name' => 'required|max:120',
+            'password' => 'required|min:6'
+        ]);
         $email = $request['email'];
         $first_name = $request['first_name'];
         $password = bcrypt($request['password']);
@@ -32,6 +36,12 @@ class UserController extends Controller
 
     public function postSignIn(Request $request)
     {
+
+           $this->validate($request, [
+              'email' => 'required',
+               'password' => 'required'
+           ]);
+
         if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])){
             return redirect()->route('dashboard');
         }
