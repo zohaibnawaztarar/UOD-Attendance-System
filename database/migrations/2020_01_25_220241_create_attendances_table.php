e2cf3156e200a2e38c4d08b96957bff4e28e819c
Zohaib Nawaz Tarar <zohaib_nawaz41@yahoo.com>
2020-03-03 14:17:06 +0000

    Now Students can signin

---
+++ database/migrations/2020_01_25_220241_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('session_id');
+            $table->foreign('session_id')->references('id')->on('modules')->onDelete('cascade');
+            $table->unsignedBigInteger('attendee_id');
+            $table->foreign('attendee_id')->references('id')->on('users')->onDelete('cascade');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -8,66 +8,38 @@
 @stop
 @section('content')
     @include('includes.message-block')
-    <h1>This is dashboard page for School Staff only</h1>
+    <h1>This is dashboard page for students only</h1>
 
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100 p-t-30 p-b-50">
 
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Sidebar 09</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <form action="{{route('students.attendanceSignIn')}}" method="post" class="login100-form validate-form p-b-33 p-t-5">
+                    <h2 class="text-center"> Sign Attendance </h2>
+                    <div class="wrap-input100 validate-input" data-validate = "Enter PIN">
+                        <input class="input100" type="text" name="pin" placeholder="PIN" id="pin" value="{{Request::old('pin')}}">
+                        <span class="focus-input100" data-placeholder="&#xe818;"></span>
+                        <input type="hidden" name="attendee_id" value="{{Auth::user()->id}}">
+                        <input type="hidden" name="status" value="1">
+                        <input type="hidden" name="session_id" value="2">
+                        {{--{{ route('lecturers', ['teach_id' => Auth::user()->id]) }}--}}
+                    </div>
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+                    <div class="container-login100-form-btn m-t-32">
+                        <button type="submit" class="login100-form-btn">
+                            Sign Now
+                        </button>
+                        <input type="hidden" name="_token" value="{{Session::token()}}">
+                    </div>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../../css/style.css">
+                </form>
 
-</head>
-<body>
-<div class="wrapper d-flex align-items-stretch">
-    <nav id="sidebar">
-        <div class="custom-menu">
-            <button type="button" id="sidebarCollapse" class="btn btn-primary">
-            </button>
-        </div>
-        <div class="img bg-wrap text-center py-4" style="background-image: url(../../public/images/bg_1.jpg);">
-            <div class="user-logo">
-                <div class="img" style="background-image: url(../../public/images/logo.jpg);"></div>
-                <h3>Student</h3>
             </div>
         </div>
-        <ul class="list-unstyled components mb-5">
-            <li class="active">
-                <a href="#"><span class="fa fa-home mr-3"></span> Home</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-download mr-3 notif"><small class="d-flex align-items-center justify-content-center">5</small></span> Link 1</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-gift mr-3"></span> Link 2</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-trophy mr-3"></span> Link 3</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-cog mr-3"></span> Link 4</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-support mr-3"></span> Link 5</a>
-            </li>
-            <li>
-                <a href="#"><span class="fa fa-sign-out mr-3"></span> Sign Out</a>
-            </li>
-        </ul>
-
-    </nav>
-
-    <!-- Page Content  -->
-    <div id="content" class="p-4 p-md-5 pt-5">
-        <h2 class="mb-4">Sidebar #09</h2>
+    </div>
 
 
+    {{--=======================================QR Code Scanner=======================================================--}}
         <script src={{asset('js/instascan.min.js')}}></script>
 
         <video id="preview"></video>
@@ -88,11 +60,10 @@
             });
         </script>
 
+        {{--=======================================QR Code Scanner=======================================================--}}
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-</div>
+
+
 
 
 
@@ -100,6 +71,6 @@
 <script src="../js/popper.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 <script src="../js/main.js"></script>
-</body>
-</html>
+
+
 @endsection
